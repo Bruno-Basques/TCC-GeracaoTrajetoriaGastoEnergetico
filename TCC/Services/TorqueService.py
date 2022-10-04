f4ff68526962f9a17dd9a960c976424a2040b0f5
Bruno Augusto Basques Ramos Camargo Rodrigues <brunocr@ciandt.com>
2022-10-04 00:10:41 -0300

    Correção do cálculo de torque, substituindo os valores reais da velocidade e aceleração por absolutos na conta do torque total.

--- TCC/Services/TorqueService.py
+++ TCC/Services/TorqueService.py
@@ -1,12 +1,11 @@
 from math import cos, sin
 
 
-def calculo_torque_elo1(tempo_discretizacao_somatorio, elo1_inercia, elo1_massa, elo1_comprimento,
-                                  elo2_inercia, elo2_massa, elo2_comprimento,
-                                  lista_pontos_interm_espaco_juntas1, lista_pontos_interm_espaco_juntas2,
-                                  lista_aceleracoes_angulares_theta1, lista_aceleracoes_angulares_theta2,
-                                  lista_velocidades_angulares_theta1, lista_velocidades_angulares_theta2,
-                                  motor_torque_atrito, motor_momento_inercia, motor_coeficiente_atrito_viscoso):
+def calculo_torque_elo1(tempo_discretizacao_somatorio, elo1_inercia, elo1_massa, elo1_comprimento, elo2_inercia, elo2_massa,
+                        elo2_comprimento, lista_pontos_interm_espaco_juntas1, lista_pontos_interm_espaco_juntas2,
+                        lista_aceleracoes_angulares_theta1, lista_aceleracoes_angulares_theta2, lista_velocidades_angulares_theta1,
+                        lista_velocidades_angulares_theta2, motor_torque_atrito, motor_momento_inercia,
+                        motor_coeficiente_atrito_viscoso):
     """
     A função "calculo_torque_elo1" calcula o torque necessário para movimentar o elo 1, considerando a inércia e
     outras propriedades do elo 2.
@@ -23,8 +22,6 @@ def calculo_torque_elo1(tempo_discretizacao_somatorio, elo1_inercia, elo1_massa,
     motor_coeficiente_atrito_viscoso = Coeficiente de atrito viscoso do motor, em kg*m²/s;
     torq_1 = Torque necessário para movimentar o conjunto dos elos 1 e 2, em N*m;
     torq_gera1 = Torque eletromagnético líquido performado pelo motor 1, em N*m;
-
-
     """
     aceleracao_gravidade = 9.81
     list_torq_gera1 = list()
@@ -45,19 +42,16 @@ def calculo_torque_elo1(tempo_discretizacao_somatorio, elo1_inercia, elo1_massa,
                                                           elo1_comprimento * (0.5 * elo1_massa + elo2_massa) * cos(
                     lista_pontos_interm_espaco_juntas1[a])) * aceleracao_gravidade
 
-        torq_gera1 = motor_torque_atrito + torq_1 + motor_momento_inercia * lista_aceleracoes_angulares_theta1[a] + \
-            motor_coeficiente_atrito_viscoso * lista_velocidades_angulares_theta1[a]
+        torq_gera1 = motor_torque_atrito + abs(torq_1) + motor_momento_inercia * abs(lista_aceleracoes_angulares_theta1[a]) + \
+            motor_coeficiente_atrito_viscoso * abs(lista_velocidades_angulares_theta1[a])
         list_torq_gera1.append(torq_gera1)
-    return   list_torq_gera1
+    return list_torq_gera1
 
-def calculo_torque_elo2(tempo_discretizacao_somatorio, elo1_comprimento,
-                                  elo2_inercia, elo2_massa, elo2_comprimento,
-                                  lista_pontos_interm_espaco_juntas1, lista_pontos_interm_espaco_juntas2,
-                                  lista_aceleracoes_angulares_theta1, lista_aceleracoes_angulares_theta2,
-                                  lista_velocidades_angulares_theta2,
-                                  motor_torque_atrito, motor_momento_inercia, motor_coeficiente_atrito_viscoso,
-                                  motor_constante_torque, motor_resistencia_armadura, motor_indutancia_armadura,
-                                  infinitesimal_discretizacao, motor_constante_proporcionalidade_tensao_velocidade):
+
+def calculo_torque_elo2(tempo_discretizacao_somatorio, elo1_comprimento, elo2_inercia, elo2_massa, elo2_comprimento,
+                        lista_pontos_interm_espaco_juntas1, lista_pontos_interm_espaco_juntas2, lista_aceleracoes_angulares_theta1,
+                        lista_aceleracoes_angulares_theta2, lista_velocidades_angulares_theta2, motor_torque_atrito,
+                        motor_momento_inercia, motor_coeficiente_atrito_viscoso):
     """
     A função "calculo_torque_e_energia_elo2" calcula a energia gasta para movimentar o elo 2.
     Variáveis:
@@ -71,12 +65,6 @@ def calculo_torque_elo2(tempo_discretizacao_somatorio, elo1_comprimento,
     motor_torque_atrito = Torque gerado pelo atrito interno do motor, em N*m;
     motor_momento_inercia = Momento de inércia do motor, em kg*m²;
     motor_coeficiente_atrito_viscoso = Coeficiente de atrito viscoso do motor, em kg*m²/s;
-    motor_constante_torque = Constante de torque do motor, em N.m/A;
-    motor_resistencia_armadura = Resistência elétrica da armadura do motor, em ohm;
-    motor_indutancia_armadura = Indutância elétrica da armadura do motor, em H;
-    infinitesimal_discretizacao = Infinitezimal de tempo utilizado para discretizar uma integral em um somatório, em s;
-    motor_constante_proporcionalidade_tensao_velocidade = Constante de proporcionalidade entre a tensão aplicada e a
-                                                          velocidade de rotação do motor;
     torq_2 = Torque necessário para movimentar o elo 2;
     torq_gera2 = Torque eletromagnético líquido performado pelo motor 2, em N*m;
     corr_arma2 = Corrente elétrica que percorre a armadura do motor 2, em A;
@@ -97,7 +85,7 @@ def calculo_torque_elo2(tempo_discretizacao_somatorio, elo1_comprimento,
                  cos(lista_pontos_interm_espaco_juntas1[c] + lista_pontos_interm_espaco_juntas2[c]) * \
                  aceleracao_gravidade
 
-        torq_gera2 = motor_torque_atrito + torq_2 + motor_momento_inercia * lista_aceleracoes_angulares_theta2[
-            c] + motor_coeficiente_atrito_viscoso * lista_velocidades_angulares_theta2[c]
+        torq_gera2 = motor_torque_atrito + abs(torq_2) + motor_momento_inercia * abs(lista_aceleracoes_angulares_theta2[
+            c]) + motor_coeficiente_atrito_viscoso * abs(lista_velocidades_angulares_theta2[c])
         list_torq_gera2.append(torq_gera2)
     return list_torq_gera2
