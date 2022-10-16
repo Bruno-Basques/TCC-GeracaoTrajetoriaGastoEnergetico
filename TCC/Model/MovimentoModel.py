041d6257dcdc6ddd8d5e599bd180044b2f3747c9
Bruno Augusto Basques Ramos Camargo Rodrigues <brunocr@ciandt.com>
2022-10-16 18:25:53 -0300

    Adição da descrição do modelo de Movimento.

--- TCC/Model/MovimentoModel.py
+++ TCC/Model/MovimentoModel.py
@@ -6,6 +6,17 @@ class Movimento:
                  theta2_final,
                  tempo_total,
                  infinitesimal):
+        """
+        O modelo "Movimento" tem como entrada a posição inicial, final das duas juntas, o tempo de movimento e o valor
+        utilizado para se discretizar as integrais.
+        theta1_inicial = posição inicial da junta n, em radianos;
+        theta1_final = posição final da junta n, em radianos;
+        theta2_inicial = posição inicial da junta n, em radianos;
+        theta2_final = posição final da junta n, em radianos;
+        tempo_total = tempo total de duração do movimento, em segundos;
+        infinitesimal = valor utilizado para se discretizar as integrais;
+        tempo_somatorio = valor utilizado para limitar as iterações aplicadas na discretização de integrais;
+        """
         self.__theta1_inicial = theta1_inicial
         self.__theta1_final = theta1_final
         self.__theta2_inicial = theta2_inicial
